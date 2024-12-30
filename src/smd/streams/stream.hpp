df8ab7e34cc263eab1b38f83eacbd403f3b03566
Steve Downey <sdowney@gmail.com>
2024-12-29 20:39:38 -0500

    Initial working build

---
+++ src/smd/streams/stream.hpp
@@ -0,0 +1,181 @@
+#include <cassert>
+#include <cstdlib>
+#include <utility>
+#include <functional>
+
+// MAYBE
+/*
+nothing = λ. λn.λj.n
+   just = λx.λn.λj.j x
+*/
+
+inline constexpr auto nothing = []() { return [](auto n) { return [n](auto _) { return n(); }; }; };
+
+inline constexpr auto just = [](auto x) { return [x](auto _) { return [x](auto j) { return j(x); }; }; };
+
+// inline constexpr auto nothing = []() {
+//     return [](auto n, auto _) { return n(); };
+// };
+
+// inline constexpr auto just = [](auto x) {
+//     return [x](auto _, auto j) { return j(x); };
+// };
+
+inline constexpr auto isNothing = [](auto m) { return m([]() { return true; })([](auto _) { return false; }); };
+inline constexpr auto isJust    = [](auto m) { return m([]() { return false; })([](auto _) { return true; }); };
+inline constexpr auto fromJust  = [](auto m) { return m([]() { std::abort(); })([](auto x) { return x; }); };
+inline constexpr auto maybe     = [](auto d) {
+    return [d](auto f) { return [d, f](auto m) { return m([d]() { return d; })(f); }; };
+};
+
+
+// EITHER
+/*
+    left   = λa.λl.λr.l a
+    right  = λb.λl.λr.r b
+
+    either = λl.λr.λe.e l r
+*/
+
+inline constexpr auto left  = [](auto a) { return [a](auto l) { return [l, a](auto _) { return l(a); }; }; };
+inline constexpr auto right = [](auto b) { return [b](auto _) { return [b](auto r) { return r(b); }; }; };
+
+inline constexpr auto isLeft = [](auto e) { return e([](auto _) { return true; })([](auto _) { return false; }); };
+;
+inline constexpr auto isRight = [](auto e) { return e([](auto _) { return false; })([](auto _) { return true; }); };
+;
+
+inline constexpr auto fromLeft = [](auto d) {
+    return [d](auto e) { return e([](auto x) { return x; })([d](auto _) { return d; }); };
+};
+inline constexpr auto fromRight = [](auto d) {
+    return [d](auto e) { return e([d](auto _) { return d; })([](auto x) { return x; }); };
+};
+
+inline constexpr auto either = [](auto l) { return [l](auto r) { return [l, r](auto e) { return e(l)(r); }; }; };
+
+
+// EITHER2
+/*
+    left   = λa.λl r.l a
+    right  = λb.λl r.r b
+
+    either = λl r.λe.e l r
+*/
+namespace either2 {
+inline constexpr auto left  = [](auto a) { return [a](auto l, auto _) { return l(a); }; };
+inline constexpr auto right = [](auto b) { return [b](auto _, auto r) { return r(b); }; };
+
+inline constexpr auto isLeft = [](auto e) { return e([](auto _) { return true; }, [](auto _) { return false; }); };
+;
+inline constexpr auto isRight = [](auto e) { return e([](auto _) { return false; }, [](auto _) { return true; }); };
+;
+
+inline constexpr auto fromLeft = [](auto d, auto e) { return e([](auto x) { return x; }, [d](auto _) { return d; }); };
+inline constexpr auto fromRight = [](auto d, auto e) {
+    return e([d](auto _) { return d; }, [](auto x) { return x; });
+};
+
+inline constexpr auto either = [](auto l, auto r) { return [l, r](auto e) { return e(l, r); }; };
+} // namespace either2
+// EITHER 3 -- Case Record
+namespace either3 {
+template <typename A, typename B, typename R>
+struct EitherCase {
+    std::function<R(A)> left;
+    std::function<R(B)> right;
+    constexpr EitherCase(std::function<R(A)> left, std::function<R(B)> right) : left(left), right(right) {}
+};
+
+template <typename A, typename B>
+struct Either {
+    inline static constexpr auto left = [](A a) {
+        return [a]<typename R>(EitherCase<A, B, R> c) { return c.left(a); };
+    };
+
+    inline static constexpr auto right = [](B b) {
+        return [b]<typename R>(EitherCase<A, B, R> c) { return c.right(b); };
+    };
+
+    template <typename R>
+    auto operator()(EitherCase<A, B, R> c) -> R;
+};
+
+template <typename A, typename B>
+inline constexpr auto isLeft =
+    [](auto e) { return e(EitherCase<A, B, bool>{[](auto _) { return true; }, [](auto _) { return false; }}); };
+
+template <typename A, typename B>
+inline constexpr auto isRight =
+    [](auto e) { return e(EitherCase<A, B, bool>{[](auto _) { return false; }, [](auto _) { return true; }}); };
+
+template <typename A, typename B>
+inline constexpr auto fromLeft = [](auto d, auto e) {
+    return e(EitherCase<A, B, decltype(d)>{[](auto x) { return x; }, [d](auto _) { return d; }});
+};
+
+template <typename A, typename B>
+inline constexpr auto fromRight = [](auto d, auto e) {
+    return e(EitherCase<A, B, decltype(d)>{[d](auto _) { return d; }, [](auto x) { return x; }});
+};
+
+template <typename A, typename B, typename R>
+inline constexpr auto either =
+    [](std::function<R(A)> l, std::function<R(B)> r) { return [l, r](auto e) { return e(EitherCase{l, r}); }; };
+
+} // namespace either3
+
+// PAIR
+
+/*
+    pair = λl r p. p l r
+    fst = λp. p (λl r. l)
+    snd = λp. p (λl r. r)
+*/
+
+inline constexpr auto pair = [](auto l, auto r) { return [l, r](auto p) { return p(l, r); }; };
+
+inline constexpr auto fst = [](auto p) { return p([](auto l, auto r) { return l; }); };
+
+inline constexpr auto snd = [](auto p) { return p([](auto l, auto r) { return r; }); };
+
+
+// LIST
+/*
+    nil  =         λn c . n
+    cons = λx xs . λn c . c x xs
+
+    isnil  = λl . l (λx xs . false) true
+    head   = λl . l nil (λx xs . x)
+    tail   = λl . l nil (λx xs . xs)
+*/
+
+template <typename A, typename AS, typename R>
+struct ScottCase {
+    const std::function<R(A, AS)>& nil;
+    const std::function<R(A, AS)>& cons;
+};
+
+inline constexpr auto Nil = [](auto nil, auto cons) { return nil(); };
+
+inline constexpr auto Cons = [](auto x, auto xs) { return [x, xs](auto nil, auto cons) { return cons(x, xs); }; };
+
+inline constexpr auto isNil = [](auto l) -> bool {
+    return l([]() { return true; }, [](auto x, auto xs) { return false; });
+};
+
+inline constexpr auto head = [](auto l) { return l([]() { std::abort(); }, [](auto x, auto xs) { return x; }); };
+
+inline constexpr auto tail = [](auto l) { return l([]() { std::abort(); }, [](auto x, auto xs) { return xs; }); };
+
+// List functions
+/*
+ */
+// inline constexpr auto take = [](this const auto& self, auto n, auto l) {
+//     if (isNil(l))
+//         return Nil;
+//     else if (n == 0)
+//         return Nil;
+//     else
+//         return Cons(head(l), self(n - 1, tail(l)));
+// };
---
+++ src/smd/streams/stream.t.cpp
@@ -0,0 +1,168 @@
+#include <smd/streams/stream.hpp>
+
+void maybe_test() {
+    constexpr auto m1 = nothing();
+    constexpr auto m2 = just(5);
+
+    constexpr bool b1 = isNothing(m1);
+    constexpr bool b2 = isJust(m2);
+    constexpr bool b3 = isNothing(m2);
+    constexpr bool b4 = isJust(m1);
+    static_assert(b1 == true);
+    static_assert(b2 == true);
+    static_assert(b3 == false);
+    static_assert(b4 == false);
+
+    constexpr int k = fromJust(m2);
+    static_assert(k == 5);
+
+    //    constexpr int k2 = fromJust(m1);
+
+    constexpr int j = maybe(7)([](int i) { return 2 * i; })(m2);
+    constexpr int l = maybe(7)([](int i) { return 2 * i; })(m1);
+    static_assert(j == 10);
+    static_assert(l == 7);
+
+    return;
+}
+
+void either_test() {
+    constexpr auto l  = left(7);
+    constexpr auto r  = right(9.0);
+    constexpr bool b1 = isLeft(l);
+    constexpr bool b2 = isLeft(r);
+    constexpr bool b3 = isRight(l);
+    constexpr bool b4 = isRight(r);
+    static_assert(b1 == true);
+    static_assert(b2 == false);
+    static_assert(b3 == false);
+    static_assert(b4 == true);
+
+    constexpr int k1 = fromLeft(11)(l);
+    constexpr int k2 = fromLeft(11)(r);
+    static_assert(k1 == 7);
+    static_assert(k2 == 11);
+
+    constexpr double k3 = fromRight(11)(l);
+    constexpr double k4 = fromRight(11)(r);
+    static_assert(k3 == 11.0);
+    static_assert(k4 == 9.0);
+
+    constexpr auto   match = either([](auto x) -> double { return 2 * x; })([](auto x) -> double { return 3 * x; });
+    constexpr double d1    = match(l);
+    constexpr double d2    = match(r);
+    static_assert(d1 == 14.0);
+    static_assert(d2 == 27.0);
+}
+
+namespace either2 {
+void either_test() {
+    constexpr auto l  = left(7);
+    constexpr auto r  = right(9.0);
+    constexpr bool b1 = isLeft(l);
+    constexpr bool b2 = isLeft(r);
+    constexpr bool b3 = isRight(l);
+    constexpr bool b4 = isRight(r);
+    static_assert(b1 == true);
+    static_assert(b2 == false);
+    static_assert(b3 == false);
+    static_assert(b4 == true);
+
+    constexpr int k1 = fromLeft(11, l);
+    constexpr int k2 = fromLeft(11, r);
+    static_assert(k1 == 7);
+    static_assert(k2 == 11);
+
+    constexpr double k3 = fromRight(11, l);
+    constexpr double k4 = fromRight(11, r);
+    static_assert(k3 == 11.0);
+    static_assert(k4 == 9.0);
+
+    constexpr auto   match = either([](auto x) -> double { return 2 * x; }, [](auto x) -> double { return 3 * x; });
+    constexpr double d1    = match(l);
+    constexpr double d2    = match(r);
+    static_assert(d1 == 14.0);
+    static_assert(d2 == 27.0);
+}
+} // namespace either2
+
+namespace either3 {
+
+void either_test() {
+    constexpr auto l  = Either<int, double>::left(7);
+    constexpr auto r  = Either<int, double>::right(9.0);
+    bool           b1 = isLeft<int, double>(l);
+    bool           b2 = isLeft<int, double>(r);
+    bool           b3 = isRight<int, double>(l);
+    bool           b4 = isRight<int, double>(r);
+    assert(b1 == true);
+    assert(b2 == false);
+    assert(b3 == false);
+    assert(b4 == true);
+
+    int k1 = fromLeft<int, double>(11, l);
+    int k2 = fromLeft<int, double>(11, r);
+    assert(k1 == 7);
+    assert(k2 == 11);
+
+    double k3 = fromRight<int, double>(11, l);
+    double k4 = fromRight<int, double>(11, r);
+    assert(k3 == 11.0);
+    assert(k4 == 9.0);
+
+    auto match =
+        either<int, double, double>([](auto x) -> double { return 2 * x; }, [](auto x) -> double { return 3 * x; });
+    double d1 = match(l);
+    double d2 = match(r);
+    assert(d1 == 14.0);
+    assert(d2 == 27.0);
+}
+} // namespace either3
+
+void pair_test() {
+    constexpr auto p1 = pair(7, 0);
+    constexpr int  k  = fst(p1);
+    static_assert(k == 7);
+    static_assert(0 == snd(p1));
+}
+
+void list_test() {
+    constexpr auto l1 = Nil;
+    constexpr auto l2 = Cons(5, Nil);
+    constexpr auto l3 = Cons(6, l2);
+    static_assert(isNil(l1) == true);
+    static_assert(isNil(l2) == false);
+    static_assert(head(l2) == 5);
+    static_assert(isNil(tail(l2)));
+    static_assert(head(l3) == 6);
+    static_assert(head(tail(l3)) == 5);
+}
+
+// List functions
+/*
+ */
+// inline constexpr auto take = [](this const auto& self, auto n, auto l) {
+//     if (isNil(l))
+//         return Nil;
+//     else if (n == 0)
+//         return Nil;
+//     else
+//         return Cons(head(l), self(n - 1, tail(l)));
+// };
+
+void list_function_test() {
+    constexpr auto l1 = Nil;
+    constexpr auto l2 = Cons(5, Nil);
+    constexpr auto l3 = Cons(6, l2);
+    // constexpr auto l  = take(1, l3);
+    // static_assert(head(l) == 5);
+}
+
+int all_main() {
+    maybe_test();
+    either2::either_test();
+    either3::either_test();
+    pair_test();
+    list_test();
+    return 0;
+}
