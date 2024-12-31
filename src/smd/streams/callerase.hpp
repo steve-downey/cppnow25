dec1054744600531b4d1943c488a672f31beda4c
Steve Downey <sdowney@gmail.com>
2024-12-31 18:30:11 -0500

    Type erased constexpr replacement for `function`

---
+++ src/smd/streams/callerase.hpp
@@ -0,0 +1,64 @@
+// callerase.hpp                                                      -*-C++-*-
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+#ifndef INCLUDED_SMD_STREAMS_CALLERASE
+#define INCLUDED_SMD_STREAMS_CALLERASE
+
+#include <memory>
+
+namespace smd {
+namespace streams {
+
+
+template <typename>
+struct callerase;
+
+template <typename R, typename... Args>
+struct callerase<R(Args...)> {
+    template <typename Callable>
+    constexpr callerase(Callable callable) : ptr{std::make_unique<implementation<Callable>>(callable)} {}
+
+    constexpr auto operator()(Args... args) const -> R { return (*ptr)(args...); }
+
+  private:
+    struct interface {
+        constexpr virtual auto operator()(Args...) -> R = 0;
+        constexpr virtual ~interface()           = default;
+    };
+
+    template <typename Callable>
+    struct implementation final : interface {
+        constexpr explicit(true) implementation(Callable callable) : callable{callable} {}
+        constexpr auto operator()(Args... args) -> R { return callable(args...); }
+
+      private:
+        Callable callable;
+    };
+
+    std::unique_ptr<interface> ptr;
+};
+
+// https://en.cppreference.com/w/cpp/utility/functional/function/deduction_guides
+
+template <class>
+struct callerase_traits {};
+
+template <class R, class G, class... A>
+struct callerase_traits<R (G::*)(A...) const> {
+    using callerase_type = R(A...);
+};
+
+template <class Callable>
+using callerase_type_t = typename callerase_traits<Callable>::callerase_type;
+
+// This overload participates in overload resolution only if &Callable::operator() is
+// well-formed when treated as an unevaluated operand and
+// decltype(&Callable::operator()) is of the form R(G::*)(A...) (optionally
+// cv-qualified, optionally noexcept, optionally lvalue reference qualified).
+// The deduced type is std::function<R(A...)>.
+template <typename Callable>
+callerase(Callable) -> callerase<callerase_type_t<decltype(&Callable::operator())>>;
+
+} // namespace streams
+} // namespace smd
+
+#endif
---
+++ src/smd/streams/callerase.t.cpp
@@ -0,0 +1,31 @@
+// src/smd/streams/callerase.t.cpp                      -*-C++-*-
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#include <smd/streams/callerase.hpp>
+
+#include <smd/streams/callerase.hpp> // test 2nd include OK
+
+#include <gtest/gtest.h>
+
+
+consteval auto test_empty() {
+    smd::streams::callerase f = [] { return 42; };
+    return f();
+}
+
+consteval auto test_arg() {
+    smd::streams::callerase f = [](int i) { return i; };
+    return f(42);
+}
+
+consteval auto test_capture() {
+    int      i = 42;
+    smd::streams::callerase f = [&] { return i; };
+    return f();
+}
+
+TEST(CallEraseTest, Breathing) {
+    static_assert(42 == test_empty());
+    static_assert(42 == test_arg());
+    static_assert(42 == test_capture());
+}
