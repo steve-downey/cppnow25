a93abdf3d5a1ce26836bbba676dd3b7c6494592a
Steve Downey <sdowney2@bloomberg.net>
2025-04-26 09:57:45 -0400

    Maybe examples

--- streams/code/examples/maybe.cpp
+++ streams/code/examples/maybe.cpp
@@ -1,1 +1,38 @@
-int main() { return 0; }
+#include <iostream>
+#include <smd/streams/maybe.hpp>
+
+auto fmapMaybe = [](auto f) {
+        return [f](auto m) {
+                return m([]() { return smd::streams::nothing(); })(
+                        [f](auto j) { return smd::streams::just(f(j)); });
+        };
+};
+
+int main() {
+        auto t1 = fmapMaybe([](auto k) { return k + 1; });
+        auto t2 = fmapMaybe([](auto k) { return k * 3; });
+
+        constexpr auto m1 = smd::streams::nothing();
+        constexpr auto m2 = smd::streams::just(5);
+
+        auto r1 = t1(m1);
+        auto r2 = t2(m1);
+        auto r3 = t1(m2);
+        auto r4 = t2(m2);
+
+        std::cout << std::boolalpha << "r1: " << smd::streams::isJust(r1)
+                  << '\n';
+        std::cout << std::boolalpha << "r2: " << smd::streams::isJust(r2)
+                  << '\n';
+        std::cout << std::boolalpha << "r3: " << smd::streams::isJust(r3)
+                  << '\n';
+        std::cout << std::boolalpha << "r4: " << smd::streams::isJust(r4)
+                  << '\n';
+
+        std::cout << std::boolalpha << "r3: " << smd::streams::fromJust(r3)
+                  << '\n';
+        std::cout << std::boolalpha << "r4: " << smd::streams::fromJust(r4)
+                  << '\n';
+
+        return 0;
+}
