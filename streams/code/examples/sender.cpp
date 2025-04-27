94957957927c478887ded0f9220742969dbc911c
Steve Downey <sdowney@gmail.com>
2025-04-26 20:03:41 -0400

    Pair sender example

--- streams/code/examples/sender.cpp
+++ streams/code/examples/sender.cpp
@@ -2,6 +2,7 @@
 #include <smd/streams/callerase.hpp>
 
 #include <iostream>
+#include <cassert>
 
 namespace ex = beman::execution;
 
@@ -53,22 +54,40 @@ template <typename Value> struct sender2 {
 static_assert(ex::sender<sender2<int>>);
 static_assert(ex::sender<sender2<std::string>>);
 
+/*
+nothing = λ. λn.λj.n
+   just = λx.λn.λj.j x
+*/
+
 inline constexpr auto nothing = []() {
-        return [](auto n, auto _) { return n(); };
+        return [](auto n) { return [n](auto _) { return n(); }; };
 };
 
 inline constexpr auto just = [](auto x) {
-        return [x](auto _, auto j) { return j(x); };
+        return [x](auto _) { return [x](auto j) { return j(x); }; };
 };
 
-inline const smd::streams::callerase n = []() {
-        return [](auto n, auto _) { return n(); };
+/*
+    pair = λl r p. p l r
+    fst = λp. p (λl r. l)
+    snd = λp. p (λl r. r)
+*/
+
+// 85dedb42-2c9c-43d5-8157-edc8dbc1df34
+inline constexpr auto pair = [](auto l, auto r) {
+        return [l, r](auto p) { return p(l, r); };
+};
+// 85dedb42-2c9c-43d5-8157-edc8dbc1df34 end
+
+// d7eb7f23-6f65-42c7-9814-9f85163d755f
+inline constexpr auto fst = [](auto p) {
+        return p([](auto l, auto r) { return l; });
 };
 
-inline const smd::streams::callerase j = [](int x) {
-        return [x](auto _, auto j) { return j(x); };
+inline constexpr auto snd = [](auto p) {
+        return p([](auto l, auto r) { return r; });
 };
-
+// d7eb7f23-6f65-42c7-9814-9f85163d755f end
 
 int main() {
         using namespace std::string_literals;
@@ -77,4 +96,19 @@ int main() {
         assert(result);
         assert(*result == std::tuple(17));
         std::cout << std::get<0>(*result) << '\n';
+
+        ex::sender auto pair = ex::just(3.14, 42);
+        auto fst = [](ex::sender auto p) -> ex::sender auto {
+                return ex::then(p, [](auto l, auto r) { return l; });
+        };
+
+        auto snd = [](ex::sender auto p) -> ex::sender auto {
+                return ex::then(p, [](auto l, auto r) { return r; });
+        };
+
+        auto result2 = ex::sync_wait(fst(pair));
+        std::cout << std::get<0>(*result2) << '\n';
+
+        auto result3 = ex::sync_wait(snd(pair));
+        std::cout << std::get<0>(*result3) << '\n';
 }
